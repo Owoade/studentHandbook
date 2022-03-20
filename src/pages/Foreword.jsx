b9acd842ed923e846f56845d6b3e8df08f6b050f
Owoade <63651250+Owoade@users.noreply.github.com>
2022-03-21 00:36:09 +0100

    new build

---
+++ src/pages/Foreword.jsx
@@ -0,0 +1,11 @@
+import PageWrapper from "../components/PageWrapper";
+import Foreword from "../components/Home/Foreword";
+const Foreword = () => {
+    return ( 
+        <PageWrapper>
+            <Foreword />
+        </PageWrapper>
+     );
+}
+ 
+export default Foreword;
